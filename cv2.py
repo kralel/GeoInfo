8f2dfa1c68fe29cac416d5b4638179d99d7a62b0
kralel <kralovaeliska3@natur.cuni.cz>
2024-10-10 12:13:36 +0200

    minmax - bez funkce, s funkcí

---
+++ cv2.py
@@ -0,0 +1,13 @@
+X = [1, 4, 58, 17, -9]
+
+#for i in range(0, len(X)):
+#    print(X[i])
+    
+#for i in range(len(X)):
+#    print(X[i])
+    
+#for i in range(10,1,-3):
+#    print(X[i])
+    
+for x in X:
+    print(x)
