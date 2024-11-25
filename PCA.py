6363436283fa9f833dfa7bba2cc8d7e76d16d1fd
kralel <kralovaeliska3@natur.cuni.cz>
2024-11-25 22:58:56 +0100

    PCA+info

--- PCA.py
+++ PCA.py
@@ -10,5 +10,14 @@ matrix = np.asarray([X, Y])
 #Compute a covariance matrix
 cov_matrix = np.cov(matrix)
 
+print("kovarianční matice: \n", cov_matrix)
+
 #Compute the eigenvalues and eigenvectors
 eval, evec = np.linalg.eig(cov_matrix)
+
+print("vlastní číslo: \n", eval)
+print("vlastní vektor: \n", evec)
+
+#Compute amount of information in PCA in %
+info = eval[0]/(eval[0]+eval[1])*100
+print(f"První hlavní komponenta obsahuje {info:.2f} % informace datového souboru.")    
