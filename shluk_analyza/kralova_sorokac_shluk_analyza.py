1abb877dafcb584bf822e2d4c37d705cd9407855
kralel <kralovaeliska3@natur.cuni.cz>
2024-12-11 14:25:05 +0100

    final bez dbscan

--- shluk_analyza/kralova_sorokac_shluk_analyza.py
+++ shluk_analyza/kralova_sorokac_shluk_analyza.py
@@ -7,16 +7,16 @@ import sklearn.cluster
 
 #Generate ramdom points in 5 cluster
 np.random.seed(3)
-ax = np.asarray(np.random.uniform(12, 25, 20))
-ay = np.asarray(np.random.uniform(8, 15, 20))
-bx = np.asarray(np.random.uniform(-10, 2, 20))
-by = np.asarray(np.random.uniform(-10, 5, 20))
-cx = np.asarray(np.random.uniform(2, 10, 20))
-cy = np.asarray(np.random.uniform(-2, 15, 20))
-dx = np.asarray(np.random.uniform(-5, 10, 20))
-dy = np.asarray(np.random.uniform(12, 25, 20))
-ex = np.asarray(np.random.uniform(10 , 25, 20))
-ey = np.asarray(np.random.uniform(-2, 5, 20))
+ax = np.asarray(np.random.uniform(12, 25, 30))
+ay = np.asarray(np.random.uniform(8, 15, 30))
+bx = np.asarray(np.random.uniform(-10, 2, 30))
+by = np.asarray(np.random.uniform(-10, 5, 30))
+cx = np.asarray(np.random.uniform(2, 10, 30))
+cy = np.asarray(np.random.uniform(-2, 10, 30))
+dx = np.asarray(np.random.uniform(-5, 10, 30))
+dy = np.asarray(np.random.uniform(12, 25, 30))
+ex = np.asarray(np.random.uniform(10 , 25, 30))
+ey = np.asarray(np.random.uniform(-2, 5, 30))
 
 #Join x-coordnite and y-coordinte of all points
 x = np.concatenate((ax, bx, cx, dx, ex))
@@ -24,22 +24,24 @@ y = np.concatenate((ay, by, cy, dy, ey))
 
 #print(ax, ay, bx, by, cx, cy, dx, dy, ex, ey)
 
-plt.scatter(ax, ay, c="r", s=10)
-plt.scatter(bx, by, c="b", s=10)
-plt.scatter(cx, cy, c="y", s=10)
-plt.scatter(dx, dy, c="g", s=10)
-plt.scatter(ex, ey, c="m", s=10)
+#Plot of generated points
+plt.scatter(ax, ay, c="r", s=20)
+plt.scatter(bx, by, c="b", s=20)
+plt.scatter(cx, cy, c="y", s=20)
+plt.scatter(dx, dy, c="g", s=20)
+plt.scatter(ex, ey, c="m", s=20)
 plt.show()
 
 
-#Turn coordinets into set of points for kmeans fuction
+#Turn coordinets into set of points
 points = list(zip(x, y))
 
-#Kmeans
+
+#Kmeans clustering
 kmeans = sklearn.cluster.KMeans(n_clusters = 5)
 kmeans.fit(points)
 
-plt.scatter(x, y, c=kmeans.labels_, s=10)
+plt.scatter(x, y, c=kmeans.labels_, s=20)
 plt.show()
 
 
@@ -47,14 +49,5 @@ plt.show()
 hierarchical_cluster = sklearn.cluster.AgglomerativeClustering(n_clusters = 5)
 hierarchical_cluster.fit(points)
 
-plt.scatter(x, y, c=hierarchical_cluster.labels_, s=10)
-plt.show()
-
-
-#DBSCAN
-dbscan = sklearn.cluster.DBSCAN(eps=2, min_samples=2)
-dbscan.fit(points)
-
-
-plt.scatter(x, y, c=dbscan.labels_, s=10)
+plt.scatter(x, y, c=hierarchical_cluster.labels_, s=20)
 plt.show()
